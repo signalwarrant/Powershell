a7880b0ea1bdb621a117a51d22557db9f653bc54
David Hall <david.keith.hall@gmail.com>
2019-08-26 11:56:51 -0400

    Update Get-LocalAdmins.ps1

--- Get-LocalAdmins.ps1
+++ Get-LocalAdmins.ps1
@@ -1,61 +1,75 @@
+Function Get-LocalAdmins {
 <#
-    .SYNOPSIS
-        Gets the members of the local administrators of the computer and 
-        outputs the result to a CSV file. 
+.SYNOPSIS
 
-    .REQUIREMENTS
-        None that I know of.
+Gets the members of the local administrators of the computer 
+and outputs the result to a CSV file.
 
-    .LINK
-        Source script: https://gallery.technet.microsoft.com/223cd1cd-2804-408b-9677-5d62c2964883
+.PARAMETER Computers
 
-    .Parameter
-        $Computers: Computer names of devices to query 
-        
-    .EXAMPLE
-        Get-LocalAdmins -Computers CL1,CL2
-        Get-LocalAdmins -Computers (Get-Content -Path "$env:HOMEPATH\Desktop\computers.txt")
-        Get-LocalAdmins -Computers DC,SVR8 | Format-Table -AutoSize -Wrap
-        Get-LocalAdmins -Computers DC,SVR8 | Export-Csv -Path "$env:HOMEPATH\Desktop\LocalAdmin.csv" -NoTypeInformation
+Specifies the Computer names of devices to query
+
+.INPUTS
+
+System.String. Get-LocalAdmins can accept a string value to
+determine the Computers parameter.
+
+.EXAMPLE
+
+Get-LocalAdmins -Computers CL1,CL2
+
+.EXAMPLE
+
+Get-LocalAdmins -Computers (Get-Content -Path "$env:HOMEPATH\Desktop\computers.txt")
+
+.EXAMPLE
+
+Get-LocalAdmins -Computers DC,SVR8 | Format-Table -AutoSize -Wrap
+
+.EXAMPLE
+
+Get-LocalAdmins -Computers DC,SVR8 | Export-Csv -Path "$env:HOMEPATH\Desktop\LocalAdmin.csv" -NoTypeInformation
+
+.LINK
+
+Source script: https://gallery.technet.microsoft.com/223cd1cd-2804-408b-9677-5d62c2964883
 #>
 
-
-Function Get-LocalAdmins {
-    [CmdletBinding()]
     Param(
-        [Parameter(Mandatory=$True)]
+        [Parameter(Mandatory)]
         [string[]]$Computers
         )
     # testing the connection to each computer via ping before
     # executing the script
     foreach ($computer in $Computers) {
-        if (Test-Connection -ComputerName $computer -Quiet -count 1){
+        if (Test-Connection -ComputerName $computer -Quiet -count 1) {
             Add-Content -value $computer -path $env:USERPROFILE\AppData\Local\Temp\livePCs.txt -Force
-        }else{
-            Write-Verbose -Message "$computer is unreachable" -Verbose
+        } else {
+            Write-Verbose -Message ('{0} is unreachable' -f $computer) -Verbose
         }
     }
 
     $liveComputers = Get-Content -Path $env:USERPROFILE\AppData\Local\Temp\livePCs.txt
 
     $list = new-object -TypeName System.Collections.ArrayList
-    foreach($computer in $liveComputers){
-        $admins = Get-WmiObject win32_groupuser -ComputerName $computer | Where-Object {$_.groupcomponent –like '*"Administrators"'} 
-        $obj = [pscustomobject]@{
+    foreach ($computer in $liveComputers) {
+        $admins = Get-WmiObject -Class win32_groupuser -ComputerName $computer | 
+            Where-Object {$_.groupcomponent -like '*"Administrators"'} 
+        $obj = New-Object -TypeName PSObject -Property @{
             ComputerName = $computer
             LocalAdmins = $null
         }
-        ForEach($admin in $admins){
-            $admin.partcomponent –match “.+Domain\=(.+)\,Name\=(.+)$” | out-null 
-            $matches[1].trim('"') + “\” + $matches[2].trim('"') + "`n" | out-null
-            $obj.Localadmins += $matches[1].trim('"') + “\” + $matches[2].trim('"') + "`n"
+        foreach ($admin in $admins) {
+            $null = $admin.partcomponent -match '.+Domain\=(.+)\,Name\=(.+)$' 
+            $null = $matches[1].trim('"') + '\' + $matches[2].trim('"') + "`n"
+            $obj.Localadmins += $matches[1].trim('"') + '\' + $matches[2].trim('"') + "`n"
         }
-        $list.add($obj) | Out-Null
+        $null = $list.add($obj)
     }
     $list
 
     # Cleaning up the txt files 
-    if(Test-Path -Path $env:USERPROFILE\AppData\Local\Temp\livePCs.txt){
+    if (Test-Path -Path $env:USERPROFILE\AppData\Local\Temp\livePCs.txt) {
         Remove-Item -Path $env:USERPROFILE\AppData\Local\Temp\livePCs.txt -Force
     }
 }
