0255763a56d81c7b81347f31a2f9315906469af7
David Hall <david@signalwarrant.com>
2021-10-28 16:48:51 -0400

    Update profile.ps1

--- PSAffirmaitons/profile.ps1
+++ PSAffirmaitons/profile.ps1
@@ -1,4 +1,4 @@
 # Testing Affirmations
-$restContent = Invoke-RestMethod -Uri ""
+$restContent = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/signalwarrant/Powershell/master/PSAffirmaitons/affirmations.json"
 $randomAffirmation = $restContent | Get-Random -Count 1
 $randomAffirmation
