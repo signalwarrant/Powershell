5c04334e2c05fe91831bf1cb0b26f809765748ba
David Hall <david@signalwarrant.com>
2021-12-07 10:10:20 -0500

    Update profile-Azure-Speech.ps1

--- PSAffirmaitons/profile-Azure-Speech.ps1
+++ PSAffirmaitons/profile-Azure-Speech.ps1
@@ -1,16 +1,20 @@
 $apikey = Get-Content -Path "C:\Users\Dave\Documents\key.txt"
 $uri = "https://raw.githubusercontent.com/signalwarrant/Powershell/master/PSAffirmaitons/affirmations.json"
 $mp3File = 'C:\Users\Dave\Documents\affirm.mp3'
+$voice = 'en-US-GuyNeural'
+$region = 'eastus'
 
 # Grab the Azure Cognitive services bearer token
-Get-SpeechToken -Key $apikey -Region 'eastus'
+Get-SpeechToken -Key $apikey -Region $region
 
 # Testing Affirmations
-$restContent = Invoke-RestMethod -Uri $uri 
-$text = $restContent | Get-Random -Count 1
+$restContent = Invoke-RestMethod -Uri $uri | Get-Random -Count 1
+
+# Show text on screen
+$restContent
 
 # Convert the text to speech
-Convert-TextToSpeech -Voice en-US-JennyNeural -Text $text -Path $mp3File
+Convert-TextToSpeech -Voice $voice -Text $restContent -Path $mp3File
 
 # Play the mp3
 Add-Type -AssemblyName presentationCore
@@ -18,4 +22,6 @@ $mediaPlayer = New-Object system.windows.media.mediaplayer
 $mediaPlayer.open($mp3File)
 $mediaPlayer.Play()
 
-$text
+# Clean up the mp3
+Start-Sleep -Seconds 1
+Remove-Item -Path $mp3File -Force
