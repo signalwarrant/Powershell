0f1ca8b1744eb45c288b842880aab44ead692f05
David Hall <david@signalwarrant.com>
2022-08-03 08:03:56 -0400

    Delete PSAffirmaitons directory

--- PSAffirmaitons/profile.ps1
+++
@@ -1,4 +0,0 @@
-$uri = "https://raw.githubusercontent.com/signalwarrant/Powershell/master/PSAffirmaitons/affirmations.json" 
-
-# Testing Affirmations
-Invoke-RestMethod -Uri $uri | Get-Random -Count 1
